3ac1a473c8bbb60d907d8a550e3b2056b408c9cf
valerio <corallini.valerio.1999@gmail.com>
2023-02-18 22:42:11 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a numbers: ");
+           
+            double i = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Input a numbers: ");
+
+            double j = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Input a numbers: ");
+
+            double k = Convert.ToDouble(Console.ReadLine());
+
+            double result = (i + j + k) / 3;
+
+            Console.WriteLine("Your average is: " + result);
+
+            Console.ReadLine();
+        }
+    }
+}
